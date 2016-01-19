32de07a7583f67af1830dd0327a05612b91f49b3
Prelang Builder <builder@prelang.com>
2016-01-19 11:09:33 +0000

    Generated scaffolding for model 'Element' via Rails

---
+++ app/views/elements/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @element, :id, :lasejob_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :elements
+
   resources :pictures
 
   resources :textlines
